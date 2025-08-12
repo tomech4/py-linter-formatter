91605135c44f8b95a98b95d428d094a7c8e86203
tomech <109527747+tomech4@users.noreply.github.com>
2025-08-13 00:14:57 +0200

    provide solution to format_single_linter_file

--- app/main.py
+++ app/main.py
@@ -9,8 +9,11 @@ def format_linter_error(error: dict) -> dict:
 
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
-    # write your code here
-    pass
+    return {
+        "errors": [format_linter_error(error) for error in errors if error["filename"] == file_path],
+        "path": file_path,
+        "status": ("failed" if errors else "success")
+    }
 
 
 def format_linter_report(linter_report: dict) -> list:
