5cfd971c1b2c9dc0996bc73832a184bb62d82a5d
tomech <109527747+tomech4@users.noreply.github.com>
2025-08-13 00:23:13 +0200

    provide solution to format_linter_report

--- app/main.py
+++ app/main.py
@@ -17,5 +17,4 @@ def format_single_linter_file(file_path: str, errors: list) -> dict:
 
 
 def format_linter_report(linter_report: dict) -> list:
-    # write your code here
-    pass
+    return [format_single_linter_file(file, errors) for file, errors in linter_report.items()]
