fdf7778e513e1cfa9cab126140f55dcf848a5760
tomech <109527747+tomech4@users.noreply.github.com>
2025-08-13 22:57:11 +0200

    fix format_single_linter_file as per provide comments in PR review

--- app/main.py
+++ app/main.py
@@ -13,7 +13,6 @@ def format_single_linter_file(file_path: str, errors: list) -> dict:
         "errors": [
             format_linter_error(error)
             for error in errors
-            if error["filename"] == file_path
         ],
         "path": file_path,
         "status": ("failed" if errors else "passed")
